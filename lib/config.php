a28001b33a55cb4d3f91187e5a442fe7fd639845
DD <55561937+DredDine@users.noreply.github.com>
2022-09-08 16:15:05 +0100

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -3,16 +3,8 @@ include('func.php');
 include('igfunc.php');
 date_default_timezone_set('Asia/Hong_Kong');
 error_reporting(0);
-/*
-Jika akun terkena feedback_required tenang, tinggal tunggu 24 jam ntar pulih lagi
-@theaxe.id
-*/
 
-//UBAH BAGIAN INI
-$countTarget    = '50'; //Ambil jumlah akun per target
-$sleep_1        = '5'; //Jeda per view story
-$sleep_2        = '5'; //Jeda per view story 1 akun user
-//SAMPAI SINI AJA
+$countTarget  = '200';
 
 $answerFile		= 'storyAnswer.txt'; // FIle komentar mu
 $saveFile 		= 'logData.txt'; // File log
